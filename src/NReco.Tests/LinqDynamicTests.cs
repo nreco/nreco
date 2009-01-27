607a6bf95ab36245dd43937a9f6d80c863f266a9
fedorchenko <fedorchenko@950f7e98-8e4f-11dd-86ef-f1224e8fcbc0>
2009-01-27 15:02:36 +0000

    LinqDynamic: fixing test
    git-svn-id: https://nreco.googlecode.com/svn/trunk@231 950f7e98-8e4f-11dd-86ef-f1224e8fcbc0

--- src/NReco.Tests/LinqDynamicTests.cs
+++ src/NReco.Tests/LinqDynamicTests.cs
@@ -33,10 +33,10 @@ namespace NReco.Tests {
 			NameValueContext cntx2 = new NameValueContext() { { "a", 8 }, { "b", "hell" } };
 
 			var dt = DateTime.Now;
-			for (int i = 0; i < 5000; i++) {
-				object res = eval.Eval("a>5 && a+b.Length==12", cntx);
+			for (int i = 0; i < 50000; i++) {
+				object res = eval.Eval("Convert.ToInt32(var[\"a\"])>5 && Convert.ToInt32(var[\"a\"])+var[\"b\"].ToString().Length==12", cntx);
 				Assert.AreEqual(true, res);
-				res = eval.Eval("a>5 && a+b.Length==12", cntx2);
+				res = eval.Eval("Convert.ToInt32(var[\"a\"])>5 && Convert.ToInt32(var[\"a\"])+var[\"b\"].ToString().Length==12", cntx2);
 				Assert.AreEqual(true, res);
 			}
 			Console.WriteLine(DateTime.Now.Subtract(dt).TotalSeconds.ToString());
