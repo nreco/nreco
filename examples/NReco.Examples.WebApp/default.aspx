e8b36f6679320219503de85955c425eaefafeceb
fedorchenko <fedorchenko@950f7e98-8e4f-11dd-86ef-f1224e8fcbc0>
2009-02-12 05:58:03 +0000

    Web sample update
    git-svn-id: https://nreco.googlecode.com/svn/trunk@269 950f7e98-8e4f-11dd-86ef-f1224e8fcbc0

--- examples/NReco.Examples.WebApp/default.aspx
+++ examples/NReco.Examples.WebApp/default.aspx
@@ -4,13 +4,8 @@
 </script>
 
 <asp:Content ContentPlaceHolderID="main" runat="server">
-<%=WebManager.GetService<object>("db") %>
-<br/>
 
-<%@ Register TagPrefix="CTRL" Src="~/Templates/test.ascx" TagName="test"%>
-<CTRL:test runat="server" Prv="<%$ service:aaa %>" Conn="<%$ service: db-DalcConnection %>"/>	   
+<h1>Welcome to NReco Wiki Sample!</h1>
 
-<br/><%=WebManager.BasePath %><br/><%=WebManager.BaseUrl %><br/>
-<%=PageContext %>
 
 </asp:Content>
---
+++ src/NReco.Web.Site/XmlSiteMapProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace NReco.Web.Site {
+	
+	/// <summary>
+	/// Extended version of standard XmlSiteMapProvider.
+	/// </summary>
+	public class XmlSiteMapProvider : System.Web.XmlSiteMapProvider {
+
+		public XmlSiteMapProvider() { }
+
+		public override SiteMapNode FindSiteMapNode(string rawUrl) {
+			SiteMapNode node = base.FindSiteMapNode(rawUrl);
+			if (node == null) {
+				int idx;
+				while (node==null && (idx=rawUrl.LastIndexOf('/'))>=0) {
+					rawUrl = rawUrl.Substring(0, idx);
+					node = base.FindSiteMapNode(rawUrl);
+				}
+			}
+			return node;
+		}
+
+ 
+
+	}
+}
