b29e6b65d12fd3e2af200b321f33a8ad7aca0c39
fedorchenko <fedorchenko@950f7e98-8e4f-11dd-86ef-f1224e8fcbc0>
2009-03-01 17:54:55 +0000

    Metadata services - project stubs.
    git-svn-id: https://nreco.googlecode.com/svn/trunk@335 950f7e98-8e4f-11dd-86ef-f1224e8fcbc0

---
+++ src/NReco.Metadata.Tool/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NReco.Metadata.Tool {
+	class Program {
+		static void Main(string[] args) {
+		}
+	}
+}
---
+++ src/NReco.Metadata/AssemblyMetadataExtractor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using NReco;
+using SemWeb;
+
+namespace NReco.Metadata {
+	
+	public class AssemblyMetadataExtractor : IOperation<Assembly> {
+
+
+		public void Execute(Assembly assembly) {
+
+		}
+
+	}
+}
