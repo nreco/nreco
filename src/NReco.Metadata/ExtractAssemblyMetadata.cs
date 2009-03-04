a62fbc3a88d2763ce98e2b44cf0510988466c556
fedorchenko <fedorchenko@950f7e98-8e4f-11dd-86ef-f1224e8fcbc0>
2009-03-04 07:47:58 +0000

    Build fix.
    git-svn-id: https://nreco.googlecode.com/svn/trunk@342 950f7e98-8e4f-11dd-86ef-f1224e8fcbc0

---
+++ src/NReco.Metadata/ExtractAssemblyMetadata.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using NReco;
+using SemWeb;
+
+namespace NReco.Metadata {
+	
+	public class ExtractAssemblyMetadata : IOperation<Assembly> {
+
+		public Store RdfStore { get; set; }
+
+		public ExtractAssemblyMetadata() { }
+
+		public ExtractAssemblyMetadata(Store store) {
+			RdfStore = store;
+		}
+
+		protected Entity GetEntity(Type t) {
+			string fullName = t.Namespace+"."+t.Name;
+			return new Entity(NS.NRecoType + fullName);
+		}
+
+		public void Execute(Assembly assembly) {
+			var types = assembly.GetExportedTypes();
+			foreach (var t in types) {
+				// store class hierarchy
+				RdfStore.Add(new Statement( NS.NRecoType + t.FullName, NS.RdfTypeEntity, NS.RdfsClassEntity));
+				if (t.BaseType != null)
+					RdfStore.Add(new Statement(GetEntity(t), NS.RdfsSubClassOf, GetEntity(t.BaseType) ));
+			}
+		}
+
+	}
+}
