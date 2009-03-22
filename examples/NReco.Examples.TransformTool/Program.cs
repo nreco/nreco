9a72cecba07565f07be3b8753e026e610f9d5820
fedorchenko <fedorchenko@950f7e98-8e4f-11dd-86ef-f1224e8fcbc0>
2009-03-22 17:20:16 +0000

    Examples: transform tool rules.
    git-svn-id: https://nreco.googlecode.com/svn/trunk@393 950f7e98-8e4f-11dd-86ef-f1224e8fcbc0

---
+++ examples/NReco.Examples.TransformTool/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace NReco.Examples.TransformTool {
+	class Program {
+		static void Main(string[] args) {
+
+			using (StreamReader rdr = new StreamReader("sitemap.xml")) {
+				Console.WriteLine("sitemap.xml:\n----------------");
+				Console.WriteLine(rdr.ReadToEnd());
+			}
+
+			using (StreamReader rdr = new StreamReader("script.js")) {
+				Console.WriteLine("script.js:\n----------------");
+				Console.WriteLine(rdr.ReadToEnd());
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ examples/NReco.Examples.TransformTool/script.js
@@ -0,0 +1,5 @@
+var i = 2;
+/*mycode*/
+i = i*2;
+/*end-mycode*/
+alert(i);
