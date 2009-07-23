2f882d4fc47cbadcf0d6c98205525630ae16199d
fedorchenko <fedorchenko@950f7e98-8e4f-11dd-86ef-f1224e8fcbc0>
2009-07-23 07:52:15 +0000

    Web: DALC-based ASP.NET ProfileProvider
    git-svn-id: https://nreco.googlecode.com/svn/trunk@700 950f7e98-8e4f-11dd-86ef-f1224e8fcbc0

---
+++ src/NReco.Web.Site/Profile/DalcProfileStorage.cs
@@ -0,0 +1,136 @@
+﻿#region License
+/*
+ * NReco library (http://nreco.googlecode.com/)
+ * Copyright 2008,2009 Vitaliy Fedorchenko
+ * Distributed under the LGPL licence
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Reflection;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Web.Profile;
+using System.Data;
+using NI.Data.Dalc;
+
+namespace NReco.Web.Site.Profile {
+	
+	public class DalcProfileStorage : IProfileStorage {
+		public DalcManager DataManager { get; set; }
+		public ProfileSource[] Sources { get; set; }
+
+		public DalcProfileStorage() {
+		}
+
+		protected string ResolveFieldName(string propName, ProfileSource src) {
+			return src.FieldsMapping.ContainsKey(propName) ? src.FieldsMapping[propName] : propName;
+		}
+
+		public bool Delete(string userName) {
+			bool oneDeleted = false;
+			foreach (var source in Sources) {
+				var row = DataManager.Load(new Query(source.SourceName, (QField)ResolveFieldName("Username", source) == (QConst)userName));
+				if (row != null) {
+					oneDeleted = true;
+					DataManager.Delete(row);
+				}
+			}
+			return oneDeleted;
+		}
+
+		public SettingsPropertyValueCollection LoadValues(string userName, SettingsPropertyCollection props) {
+			var svc = new SettingsPropertyValueCollection();
+			// holds sourcename->data pairs
+			var loadedData = new Dictionary<string, IDictionary>();
+			foreach (SettingsProperty prop in props) {
+				EnsureDataLoaded(userName, prop.Name, loadedData);
+				var pv = new SettingsPropertyValue(prop);
+
+				object value = null; 
+				// lets try to locate property value
+				foreach (var src in Sources)
+					if (src.FieldsMapping.ContainsKey(prop.Name)) {
+						value = loadedData[src.SourceName][ ResolveFieldName(prop.Name,src) ];
+						break;
+					}
+				if (value == null || value == DBNull.Value)
+					value = prop.DefaultValue;
+				pv.PropertyValue = value;
+
+				svc.Add(pv);
+			}
+			return svc;
+		}
+
+		protected void EnsureDataLoaded(string userName, string pName, Dictionary<string,IDictionary> loadedData) {
+			foreach (var src in Sources)
+				if (src.FieldsMapping.ContainsKey(pName) && !loadedData.ContainsKey(src.SourceName)) {
+					var data = new Hashtable();
+					DataManager.Dalc.LoadRecord(data, new Query(src.SourceName, (QField)ResolveFieldName("Username", src) == (QConst)userName));
+					loadedData[src.SourceName] = data;
+				}
+		}
+
+		protected void EnsureDataLoaded(string userName, string pName, Dictionary<string, DataRow> loadedData) {
+			foreach (var src in Sources)
+				if (src.FieldsMapping.ContainsKey(pName) && !loadedData.ContainsKey(src.SourceName)) {
+					var row = DataManager.Load( new Query(src.SourceName, (QField)ResolveFieldName("Username", src) == (QConst)userName));
+					if (row == null) {
+						row = DataManager.Create(src.SourceName);
+						row[ResolveFieldName("Username", src)] = userName;
+					}
+					loadedData[src.SourceName] = row;
+				}
+		}
+
+
+		public void SaveValues(string userName, SettingsPropertyValueCollection values) {
+			// holds sourcename->datarow pairs
+			var profileRows = new Dictionary<string, DataRow>();
+			foreach (SettingsPropertyValue value in values) 
+				if (value.IsDirty) {
+					EnsureDataLoaded(userName, value.Property.Name, profileRows);
+
+					foreach (var src in Sources)
+						if (src.FieldsMapping.ContainsKey(value.Property.Name)) {
+							var dataCol = src.FieldsMapping[value.Property.Name];
+								profileRows[src.SourceName][dataCol] = ResolveFieldValue(profileRows[src.SourceName].Table.Columns[dataCol], value);
+						}
+				}
+			foreach (var entry in profileRows)
+				DataManager.Update(entry.Value);
+		}
+
+		protected object ResolveFieldValue(DataColumn col, SettingsPropertyValue value) {
+			// TODO: handle serialization option
+			if (value.PropertyValue == null || value.PropertyValue==DBNull.Value)
+				return DBNull.Value;
+			// if DB type and property type are matched return unserialized value
+			if (value.Property.PropertyType == col.DataType)
+				return value.PropertyValue;
+			return value.SerializedValue;
+		}
+
+		/// <summary>
+		/// Describes one profile values DALC data source
+		/// </summary>
+		public class ProfileSource {
+			public string SourceName { get; set; }
+			public IDictionary<string, string> FieldsMapping { get; set; }
+		}
+
+
+
+
+	}
+}
---
+++ src/NReco.Web.Site/Profile/IProfileStorage.cs
@@ -0,0 +1,33 @@
+﻿#region License
+/*
+ * NReco library (http://nreco.googlecode.com/)
+ * Copyright 2008,2009 Vitaliy Fedorchenko
+ * Distributed under the LGPL licence
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Web.Profile;
+
+namespace NReco.Web.Site.Profile {
+	
+	/// <summary>
+	/// Abstract profile data storage interface.
+	/// </summary>
+	public interface IProfileStorage {
+		bool Delete(string userName);
+		SettingsPropertyValueCollection LoadValues(string userName, SettingsPropertyCollection props);
+		void SaveValues(string userName, SettingsPropertyValueCollection values);
+	}
+
+}
