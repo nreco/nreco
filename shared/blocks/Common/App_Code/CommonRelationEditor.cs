91541793c398a10c7774b5214c96ec151c698e45
fedorchenko <fedorchenko@950f7e98-8e4f-11dd-86ef-f1224e8fcbc0>
2009-09-02 12:56:50 +0000

    Web: bugfix
    git-svn-id: https://nreco.googlecode.com/svn/trunk@804 950f7e98-8e4f-11dd-86ef-f1224e8fcbc0

--- shared/blocks/Common/App_Code/CommonRelationEditor.cs
+++ shared/blocks/Common/App_Code/CommonRelationEditor.cs
@@ -29,6 +29,7 @@ public abstract class CommonRelationEditor : ActionUserControl {
 	public string RelationSourceName { get; set; }
 	public string LFieldName { get; set; }
 	public string RFieldName { get; set; }
+	public string PositionFieldName { get; set; }
 
 	public object EntityId {
 		get { return ViewState["EntityId"]; }
@@ -39,6 +40,7 @@ public abstract class CommonRelationEditor : ActionUserControl {
 	}
 
 	public void ExecuteAfter_Select(ActionContext e) {
+		if (!(e.Args is ActionDataSource.SelectEventArgs)) return;
 		var data = ((ActionDataSource.SelectEventArgs)e.Args).Data;
 		if (data != null)
 			foreach (object o in data) {
@@ -48,6 +50,7 @@ public abstract class CommonRelationEditor : ActionUserControl {
 	}
 
 	public void ExecuteAfter_Insert(ActionContext e) {
+		if (!(e.Args is ActionDataSource.InsertEventArgs)) return;
 		EntityId = ((ActionDataSource.InsertEventArgs)e.Args).Values[EntityIdField];
 		Save();
 	}
@@ -68,15 +71,28 @@ public abstract class CommonRelationEditor : ActionUserControl {
 	protected void Save() {
 		var dalc = WebManager.GetService<IDalc>(DalcServiceName);
 		dalc.Delete(new Query(RelationSourceName, (QField)LFieldName == new QConst(EntityId)));
-		foreach (var id in GetControlSelectedIds() )
-			dalc.Insert(new Hashtable { { LFieldName, EntityId }, { RFieldName, id } }, RelationSourceName);
+		int idx = 0;
+		foreach (var id in GetControlSelectedIds() ) {
+			var data = new Hashtable { { LFieldName, EntityId }, { RFieldName, id } };
+			if (!String.IsNullOrEmpty(PositionFieldName))
+				data[PositionFieldName] = idx++;
+			dalc.Insert(data, RelationSourceName);
+		}
 	}
 
 	public string[] GetSelectedIds() {
 		// select visible ids
-		var ids = (from r in WebManager.GetService<IDalc>(DalcServiceName).Linq<DalcRecord>(RelationSourceName)
+		var ids = String.IsNullOrEmpty(PositionFieldName) ?
+			(from r in WebManager.GetService<IDalc>(DalcServiceName).Linq<DalcRecord>(RelationSourceName)
 				   where r[LFieldName] == EntityId
-				   select r[RFieldName]).ToArray<DalcValue>();
+				   select r[RFieldName]
+				   ).ToArray<DalcValue>()			
+				   :
+			(from r in WebManager.GetService<IDalc>(DalcServiceName).Linq<DalcRecord>(RelationSourceName)
+				   where r[LFieldName] == EntityId
+				   orderby r[PositionFieldName]
+				   select r[RFieldName]
+				   ).ToArray<DalcValue>();
 		return Array.ConvertAll<DalcValue, string>(ids, x => x.Value.ToString());
 	}
 
