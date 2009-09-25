7776bcbd92c94740b67aaa6d6d34ef6c2fe8ff59
fedorchenko <fedorchenko@950f7e98-8e4f-11dd-86ef-f1224e8fcbc0>
2009-09-25 05:27:51 +0000

    Lucene: QueryComposer elaboration
    git-svn-id: https://nreco.googlecode.com/svn/trunk@852 950f7e98-8e4f-11dd-86ef-f1224e8fcbc0

--- src/NReco.Lucene/QueryComposer.cs
+++ src/NReco.Lucene/QueryComposer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 using NReco;
 using Lucene.Net.Search;
@@ -12,19 +13,20 @@ namespace NReco.Lucene {
 
 		public string KeywordTemplate { get; set; }
 		public string SeparatorTemplate { get; set; }
+		static Regex LuceneCharsEscape = new Regex("[\\\\+\\-\\!\\(\\)\\:\\^\\]\\{\\}\\~\\*\\?]", RegexOptions.Singleline | RegexOptions.Compiled);
 
 		public QueryStringComposer() {
-			KeywordTemplate = "(+{0}) OR (\"{0}\") OR ({0}*)";
+			KeywordTemplate = "(+{0}) OR ({0}*)";
 			SeparatorTemplate = " OR ";
 		}
 
 		public string Provide(string keywords) {
 			var sb = new StringBuilder();
-			string[] userKeywords = keywords.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+			string[] userKeywords = keywords.Split(new[] { ' ', '\t', ',', ';' }, StringSplitOptions.RemoveEmptyEntries);
 			for (int i = 0; i < userKeywords.Length; i++) {
 				if (i > 0)
 					sb.Append(SeparatorTemplate);
-				sb.AppendFormat(KeywordTemplate, userKeywords[i].Trim());
+				sb.AppendFormat(KeywordTemplate, LuceneCharsEscape.Replace( userKeywords[i].Trim(), "\\$1" ) );
 			}
 			return sb.ToString();
 		}
