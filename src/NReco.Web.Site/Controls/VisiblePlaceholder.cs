55351538d0651a1d866775a1bb998d7aaedca19a
shigasumi <shigasumi@950f7e98-8e4f-11dd-86ef-f1224e8fcbc0>
2011-02-08 16:43:59 +0000

    VisiblePlaceholder control
    git-svn-id: https://nreco.googlecode.com/svn/trunk@1344 950f7e98-8e4f-11dd-86ef-f1224e8fcbc0

---
+++ src/NReco.Web.Site/Controls/VisiblePlaceholder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NReco.Web.Site.Controls {
+	public class VisiblePlaceholder : System.Web.UI.WebControls.PlaceHolder {
+		public override void DataBind() {
+			OnDataBinding(EventArgs.Empty);
+			if (Visible) {
+				base.DataBind();
+			}
+		}
+	}
+}
