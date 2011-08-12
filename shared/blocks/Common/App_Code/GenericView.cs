8b6682f76692c432b4c184f9c09bfd3a2f2a9422
shigasumi@gmail.com <shigasumi@gmail.com@950f7e98-8e4f-11dd-86ef-f1224e8fcbc0>
2011-08-12 08:46:50 +0000

    split GenericView to 2 views + helper
    git-svn-id: https://nreco.googlecode.com/svn/trunk@1433 950f7e98-8e4f-11dd-86ef-f1224e8fcbc0

--- shared/blocks/Common/App_Code/GenericView.cs
+++ shared/blocks/Common/App_Code/GenericView.cs
@@ -33,58 +33,7 @@ using NI.Data.Dalc;
 using NI.Data.Dalc.Web;
 using NI.Data.Dalc.Linq;
 
-public abstract class GenericView : ActionUserControl, IDataContextAware {
-	
-	bool _UseSessionDataContext = false;
-	public bool UseSessionDataContext { 
-		get { return _UseSessionDataContext; }
-		set { _UseSessionDataContext = value; }
-	}
-	
-	bool _UseViewstateDataContext = true;
-	public bool UseViewstateDataContext { 
-		get { return _UseViewstateDataContext; }
-		set { _UseViewstateDataContext = value;	}
-	}
-	
-	static ILog log = LogManager.GetLogger(typeof(GenericView));
-	
-	protected string SessionDataContextKey { 
-		get {
-			return String.Format("dataContext#{0}#{1}",Request.Url.AbsolutePath,ClientID);
-		}
-	}
-	
-	private IDictionary<string,object> _DataContext = null;
-	public IDictionary<string,object> DataContext {
-		get { 
-			if (UseSessionDataContext) {
-				var dataCntx = Session[SessionDataContextKey] as IDictionary<string,object>;
-				if (dataCntx==null) {
-					dataCntx = new Dictionary<string,object>();
-					Session[SessionDataContextKey] = dataCntx;
-				}				
-				return dataCntx;
-			} else {
-				if (_DataContext==null) {
-					_DataContext = new Dictionary<string,object>();
-				}
-				return _DataContext; 
-			}
-		}
-		set { 
-			if (UseSessionDataContext) {
-				Session[SessionDataContextKey] = value;
-			} else {
-				_DataContext = value;
-			}
-		}
-	}
-	
-	public GenericView() {
-		UseSessionDataContext = false;
-	}
-
+public abstract class GenericView : DataContextView {
 	protected override bool OnBubbleEvent(object sender, EventArgs e) {
 		// b/c generated buttons are not binded to event handler, lets catch their events
 		if (IsBubbleEventGeneratedSender(sender, e)) {
@@ -99,15 +48,8 @@ public abstract class GenericView : ActionUserControl, IDataContextAware {
 	}
 	
 	public string GetRouteUrlSafe(string routeName, IDictionary context) {
-		try {
-			return ControlExtensions.GetRouteUrl(this,routeName,context);
-		} catch (Exception ex) {
-			log.Write(LogEvent.Debug,ex);
-			return null;
-		}
+		return ViewHelper.GetRouteUrlSafe(this, routeName, context);
 	}
-	
-	#region ListView Handlers
 	
 	public int GetListViewRowCount(ListView listView) {
 		var pager = listView.GetChildren<DataPager>().FirstOrDefault();
@@ -151,8 +93,6 @@ public abstract class GenericView : ActionUserControl, IDataContextAware {
 		return groupValue;
 	}
 	
-	#endregion
-	
 	public bool IsFuzzyTrue(object o) {
 		return AssertHelper.IsFuzzyTrue(o);
 	}
@@ -172,21 +112,10 @@ public abstract class GenericView : ActionUserControl, IDataContextAware {
 	}
 	
 	public object GetControlValue(Control container, string ctrlId) {
-		var ctrl = container.FindControl(ctrlId);
-		if (ctrl==null) return null;
-		if (ctrl is ITextControl)
-			return ((ITextControl)ctrl).Text;
-		if (ctrl is ICheckBoxControl)
-			return ((ICheckBoxControl)ctrl).Checked;
-		throw new Exception("Cannot extract control value from "+ctrl.GetType().ToString());
+		return ViewHelper.GetControlValue(container, ctrlId);
 	}
 	public void SetControlValue(Control container, string ctrlId, object val) {
-		var ctrl = container.FindControl(ctrlId);
-		if (ctrl==null) return;
-		if (ctrl is ITextControl)
-			((ITextControl)ctrl).Text = Convert.ToString(val);
-		if (ctrl is ICheckBoxControl)
-			((ICheckBoxControl)ctrl).Checked = ConvertManager.ChangeType<bool>(val);
+		ViewHelper.SetControlValue(container, ctrlId, val);
 	}
 	
 	public IDictionary CastToDictionary(object o) {
@@ -196,39 +125,6 @@ public abstract class GenericView : ActionUserControl, IDataContextAware {
 			return (IDictionary)converter.Convert(o, typeof(IDictionary) );
 		return new DictionaryWrapper<string,object>( new NReco.Collections.ObjectDictionaryWrapper(o) );
 	}
-	
-	// if we initialized datacontext *before* viewstate load -> preserve those values
-	protected override void LoadViewState(object savedState) {
-		if ( (savedState is object[]) && !UseSessionDataContext && UseViewstateDataContext) {
-			var savedStateArr = (object[])savedState;
-			base.LoadViewState(savedStateArr[0]);
-			if (savedStateArr[1] is IDictionary<string, object>) {
-				var newContext = (IDictionary<string, object>)savedStateArr[1];
-				if (_DataContext != null) {
-					foreach (var d in _DataContext) {
-						newContext[d.Key] = d.Value;
-					}
-				} 
-				_DataContext = newContext;
-			}
-		} else {
-			base.LoadViewState(savedState);
-		}
-	}
-	protected override object SaveViewState() {
-		if (_DataContext != null && !UseSessionDataContext && UseViewstateDataContext) {
-			object baseState = base.SaveViewState();
-			object[] allStates = new object[2];
-			allStates[0] = baseState;
-			allStates[1] = _DataContext;
-			return allStates;
-		} else {
-			return base.SaveViewState();
-		}
-	}
-
-	
-	#region Custom Validators
 	
 	IDictionary<string,IDictionary<CustomValidator,bool>> ChooseOneGroupCounters = new Dictionary<string,IDictionary<CustomValidator,bool>>();
 	public void ChooseOneServerValidate(object source, ServerValidateEventArgs args) {
@@ -242,8 +138,4 @@ public abstract class GenericView : ActionUserControl, IDataContextAware {
 		var count = ChooseOneGroupCounters[group].Values.Where ( r=>r ).Count();
 		args.IsValid = !hasValue || count<=1;
 	}
-	
-	#endregion
-	
-	
 }
