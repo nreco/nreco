ca6091532133c61870674a70601a83f51868b1d7
fedorchenko <fedorchenko@950f7e98-8e4f-11dd-86ef-f1224e8fcbc0>
2012-01-11 14:57:50 +0000

    NReco.OGNL: Json helper - removing strange code
    git-svn-id: https://nreco.googlecode.com/svn/trunk@1523 950f7e98-8e4f-11dd-86ef-f1224e8fcbc0

--- src/NReco.OGNL/Helpers/Json.cs
+++ src/NReco.OGNL/Helpers/Json.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Web.Script.Serialization;
 
+using NReco.Collections;
 using NReco.Converting;
 
 namespace NReco.OGNL.Helpers
@@ -17,8 +18,6 @@ namespace NReco.OGNL.Helpers
 
         public static string ToJsonString(object obj)
         {
-            if (obj is IDictionary)
-                obj = ConvertManager.ChangeType<IDictionary<string, object>>(obj);
             return new JavaScriptSerializer().Serialize(obj);
         }
     }
