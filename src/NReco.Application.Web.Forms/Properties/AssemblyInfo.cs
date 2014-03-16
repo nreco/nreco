91773365e900b2972eb1c187f419e7d75eff3a48
Vitaliy Fedorchenko <fedorchenko@gmail.com>
2014-03-16 14:41:08 +0200

    NReco V2: NReco.Application and NReco.Application.Web.Forms package configuration

--- src/NReco.Application.Web.Forms/Properties/AssemblyInfo.cs
+++ src/NReco.Application.Web.Forms/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NReco.Web")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("NReco.Application.Web.Forms")]
+[assembly: AssemblyDescription("NReco WebForms application infrastructure")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Vitalii Fedorchenko")]
 [assembly: AssemblyProduct("NReco")]
-[assembly: AssemblyCopyright("Copyright © Vitaliy Fedorchenko")]
+[assembly: AssemblyCopyright("Copyright © Vitalii Fedorchenko")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
