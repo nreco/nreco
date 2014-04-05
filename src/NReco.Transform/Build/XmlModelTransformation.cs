47a9ec3e60179ce8506c115190a20181b80079a5
Vitaliy Fedorchenko <fedorchenko@gmail.com>
2014-04-05 20:32:43 +0300

    NReco.Transform: adding missed files

---
+++ src/NReco.Transform/Build/XmlModelTransformation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Resources;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+
+namespace NReco.Transform.Build {
+	
+	public class XmlModelTransformation : Task {
+
+		[Required]
+		public string RootPath { get; set; }
+
+		[Required]
+		public ITaskItem[] XmlModels { get; set; }
+
+		protected XmlModelProcessor ModelProcessor;
+
+		public XmlModelTransformation() {
+		}
+
+		public override bool Execute() {
+			ModelProcessor = new XmlModelProcessor(RootPath);
+			
+			foreach (var xmlModel in XmlModels) {
+				if (xmlModel.ItemSpec.Length > 0) {
+					var xmlModelPath = xmlModel.ItemSpec;
+					Log.LogMessage(MessageImportance.Normal, "Processing XML model {0}", xmlModelPath);
+					ModelProcessor.TransformModel(xmlModelPath);
+				}
+			}
+			
+			return true;
+		}
+
+	}
+}
