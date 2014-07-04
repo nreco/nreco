26dc4ab2808604583e40f1853b5b54ad946f4966
Vitalii Fedorchenko <fedorchenko@gmail.com>
2014-07-04 13:33:15 +0300

    NReco.Examples: DataMvc - use temp folder for sqlite db

---
+++ examples/NReco.Examples.DataMvc/Data/ConnectionString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using NI.Ioc;
+
+namespace NReco.Examples.DataMvc.Data {
+	public class ConnectionString : IFactoryComponent {
+		public string ConnectionStringTemplate { get; set; }
+
+		public object GetObject() {
+			return String.Format(ConnectionStringTemplate, System.IO.Path.GetTempPath() );
+		}
+
+		public Type GetObjectType() {
+			return typeof(string);
+		}
+	}
+}
