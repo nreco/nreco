99988b372336b0e933f3262d68cfce2325a3b152
Vitalii Fedorchenko <fedorchenko@gmail.com>
2014-08-29 12:45:39 +0300

    NReco.Application.Web: fix issue with MembershipProvider.CacheUser

--- src/NReco.Application.Web/Security/MembershipProvider.cs
+++ src/NReco.Application.Web/Security/MembershipProvider.cs
@@ -357,7 +357,7 @@ namespace NReco.Application.Web.Security {
 		protected void CacheUser(MembershipUser user, bool update) {
 			if (HttpContext.Current != null) {
 				var key = typeof(MembershipProvider).FullName + user.UserName;
-				if (!update || HttpContext.Current.Items.Contains(key))
+				if (update || !HttpContext.Current.Items.Contains(key))
 					HttpContext.Current.Items[key] = user;
 			}
 		}
