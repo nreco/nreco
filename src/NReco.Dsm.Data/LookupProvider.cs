3a86c87c29a7f873cbaef3863aa94b93af03dbe8
Vitalii Fedorchenko <fedorchenko@gmail.com>
2015-03-25 15:56:52 +0200

    NReco.Dsm.Data: LookupProvider - added dictionary-based cache for fast lookup resolution

--- src/NReco.Dsm.Data/LookupProvider.cs
+++ src/NReco.Dsm.Data/LookupProvider.cs
@@ -18,24 +18,41 @@ namespace NReco.Dsm.Data {
 
 		public string TextName { get; set; }
 
+		public bool UseCache { get; set; }
+
 		public LookupProvider(Func<IDictionary,IEnumerable<IDictionary>> dataProvider) {
 			DataProvider = dataProvider;
 			ValueName = "id";
 			TextName = "value";
+			UseCache = true;
 		}
+
+		Dictionary<string,object> localValueCache = null;
 
 		public object Resolve(object arg) {
 			if (arg != null && !(arg is IDictionary)) {
 				var argStr = Convert.ToString(arg);
 				if (argStr.Length > 0) {
+					if (UseCache) {
+						if (localValueCache==null)
+							localValueCache = new Dictionary<string,object>();
+						if (localValueCache.ContainsKey(argStr))
+							return localValueCache[argStr];
+					}
+
 					var valContext = new Dictionary<string,object>() {
 						{ValueName, arg}
 					};
 					var valRes = DataProvider(valContext);
 					if (valRes!=null) {
 						var firstEntry = valRes.FirstOrDefault();
-						if (firstEntry!=null && firstEntry[TextName]!=null)
-							return firstEntry[TextName];
+						if (firstEntry != null && firstEntry[TextName] != null) {
+							var val = firstEntry[TextName];
+							if (UseCache) {
+								localValueCache[argStr] = val;
+							}
+							return val;
+						}
 					}
 				}
 				return NotMatchedText;
